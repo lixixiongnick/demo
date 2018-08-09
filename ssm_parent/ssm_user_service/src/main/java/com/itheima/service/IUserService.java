894e5a9f95e3bb4c3f04ed8e3f7b50a3e0d50644
nick <lixixiongnike163.com>
2018-08-09 16:13:26 +0800

    7-11 project 1st commit

--- ssm_parent/ssm_user_service/src/main/java/com/itheima/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.domain.Users;
-
-public interface IUserService {
-    void register(String username,String password,String repassword,String telephone);
-    Users doLogin(String username);
-}
